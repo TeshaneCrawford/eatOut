e28802c4757edbb1c61cdf061335f531c424c708
Teshane Crawford <crawfordteshane@gmail.com>
2022-05-10 15:08:42 -0500

    first commit

---
+++ client/components/Layout/index.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Navbar from './Navbar'
+
+const Layout = ({ children }) => {
+  return (
+    <div>
+        <nav>
+            <Navbar />
+        </nav>
+        <main>
+            {children}
+        </main>
+    </div>
+  )
+}
+
+export default Layout
---
+++ client/pages/_app.js
@@ -0,0 +1,11 @@
+import '../styles/globals.css'
+
+const MyApp = ({ Component, pageProps }) => {
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  )
+}
+
+export default MyApp
